16c809db055ee9ae79c8013af0093e96d05f2310
sundeepkumarch <sundeepk.chinta@gmail.com>
2016-04-12 08:48:28 +0530

    Matchmaking

---
+++ TopCoder/src/com/mrsun/MatchMaking.java
@@ -0,0 +1,13 @@
+package com.mrsun;
+
+/**
+ * https://community.topcoder.com/stat?c=problem_statement&pm=2911&rd=5849
+ * @author sundechi
+ */
+public class MatchMaking {
+    
+    public String makeMatch(String[] namesWomen, String[] answersWomen, String[] namesMen, String[] answersMen, String queryWoman){
+        
+        return null;
+    }
+}
