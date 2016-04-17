7f8ec483732b1d70af1bb8a7314c06bb1cfa223b
sundeepkumarch <sundeepk.chinta@gmail.com>
2016-04-18 00:56:40 +0530

    JDK Changed

---
+++ Practice/src/com/mrsun/questions/Main.java
@@ -0,0 +1,13 @@
+package com.mrsun.questions;
+
+/*
+ * Find smallest positive decimal number containing only digits 0 and 1 which is divisible by N.
+ * (example: 7 -> 1001)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int N = 7;
+        
+    }
+}
